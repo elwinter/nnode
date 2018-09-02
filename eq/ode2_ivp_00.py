cbcd6539fa3487bcbc025ecefd4618a353b3a08f
Eric Winter <ewinter@stsci.edu>
2018-09-02 15:10:47 -0400

    Converted ode2_ivp_00.py to Spyder standard format.

--- eq/ode2_ivp_00.py
+++ eq/ode2_ivp_00.py
@@ -1,36 +1,73 @@
-# Sample 2nd-order ODE IVP
+"""Sample 2nd-order ODE IVP
 
-# The equation is defined on the domain [0,1], with the boundary
-# conditions defined at x=0.
+The equation is defined on the domain [0,1], with the initial conditions
+defined at x=0.
 
-# The analytical form of the equation is:
-# G(x,y,dy/dx) = d2y_dx2 - dy_dx + 2x - 1 = 0
-def Gf(x, y, dy_dx, d2y_dx2):
-    return d2y_dx2 - dy_dx + 2*x - 1
+The analytical form of the equation is:
+    G(x,y,dy/dx) = d2y_dx2 - dy_dx + 2x - 1 = 0
+"""
 
-# Initial conditions
+
+from inspect import getsource
+
+
 ic = 1
 ic1 = 1
 
-# Derivatives of ODE
+
+def Gf(x, y, dy_dx, d2y_dx2):
+    """Code for differential equation"""
+    return d2y_dx2 - dy_dx + 2*x - 1
+
 
 def dG_dyf(x, y, dy_dx, d2y_dx2):
+    """Derivative of G wrt y"""
     return 0
 
+
 def dG_dydxf(x, y, dy_dx, d2y_dx2):
+    """Derivative of G wrt dy/dx"""
     return -1
 
-def dG_d2ydx2f(x, y, dy_dx, d2y_dx2):
-    return 1
 
-# Define the analytical solution.
+def dG_d2ydx2f(x, y, dy_dx, d2y_dx2):
+    """Derivative of G wrt d2y/dx2"""
+
+
 def yaf(x):
+    """Analytical solution"""
     return x**2 + x + 1
 
-# Define the 1st analytical derivative.
+
 def dya_dxf(x):
+    """Derivative of analytical solution"""
     return 2*x + 1
 
-# Define the 2nd analytical derivative.
+
 def d2ya_dx2f(x):
+    """2nd derivative of analytical solution"""
     return 2
+
+
+if __name__ == '__main__':
+    print(getsource(Gf))
+    print('Gf(0) = ', Gf(0, 0, 0, 0))
+    print()
+    print(getsource(dG_dyf))
+    print('dG_dyf(0) = ', dG_dyf(0, 0, 0, 0))
+    print()
+    print(getsource(dG_dydxf))
+    print('dG_dydxf(0) = ', dG_dydxf(0, 0, 0, 0))
+    print()
+    print(getsource(dG_d2ydx2f))
+    print('dG_d2ydx2f(0) = ', dG_d2ydx2f(0, 0, 0, 0))
+    print()
+    print(getsource(yaf))
+    print('yaf(0) = ', yaf(0))
+    assert yaf(0) == ic
+    print()
+    print('dya_dxf(0) = ', dya_dxf(0))
+    assert dya_dxf(0) == ic1
+    print()
+    print('d2ya_dx2f(0) = ', d2ya_dx2f(0))
+    print()
