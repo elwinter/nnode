89d547fd5ee66b139003a346d0bd1958f343bbf9
Eric Winter <ewinter@stsci.edu>
2019-06-11 20:32:18 -0400

    Beefed up comments and changed test cases.

--- diff1dtrialfunction.py
+++ diff1dtrialfunction.py
@@ -5,29 +5,58 @@ problems
 
 The trial function takes the form:
 
-Yt(x,t) = A(x,t) + P(x,t)N(x,t,p)
+Yt(x,t) = A(x, t) + P(x, t)N(x, t, p)
 
 where:
 
-A(x,t) = boundary condition function that reduces to BC at boundaries
-P(x,t) = network coefficient function that vanishes at boundaries
-N(x,t,p) = scalar output of neural network with parameter vector p
+A(x, t) = boundary condition function that reduces to BC at boundaries
+P(x, t) = network coefficient function that vanishes at boundaries
+N(x, t, p) = scalar output of neural network with parameter vector p
 
 Example:
-    Create a default Diff1DTrialFunction object
-        Yt = Diff1DTrialFunction()
+    Create a default Diff1DTrialFunction object from boundary conditions
+        Yt_obj = Diff1DTrialFunction(bcf, delbcf, del2bcf)
+
+    Compute the value of the trial function at a given point
+        Yt = Yt_obj.Ytf([x, t], N)
+
+    Compute the value of the boundary condition function at a given point
+        A = Yt_obj.Af([x, t])
 
 Notes:
-    * Function names end in 'f'.
+    Variables that end in 'f' are usually functions or arrays of functions.
 
 Attributes:
-    None
+    bcf - 2x2 array of BC functions at (x,t)=0|1
+    delbcf - 2x2x2 array of BC gradient functions at (x,t)=0|1
+    del2bcf - 2x2x2 array of BC Laplacian component functions at (x,t)=0|1
 
 Methods:
-    None
+    Af([x, t]) - Compute boundary condition function at [x, t]
+
+    delAf([x, t]) - Compute boundary condition function gradient at [x, t]
+
+    del2Af([x, t]) - Compute boundary condition function Laplacian components
+        at [x, t]
+
+    Pf([x, t]) - Compute network coefficient function at [x, t]
+
+    delPf([x, t]) - Compute network coefficient function gradient at [x, t]
+
+    del2Pf([x, t]) - Compute network coefficient function Laplacian components
+        at [x, t]
+
+    Ytf([x, t], N) - Compute trial function at [x, t] with network output N
+
+    delYtf([x, t], N, delN) - Compute trial function gradient at [x, t] with
+        network output N and network output gradient delN.
+
+    del2Ytf([x, t], N, delN, del2N) - Compute trial function Laplacian
+        components at [x, t] with network output N, network output gradient
+        delN, and network output Laplacian components del2N
 
 Todo:
-    * Expand basic functionality
+
 """
 
 
@@ -140,6 +169,8 @@ class Diff1DTrialFunction():
 
 # Self-test code
 
+# The code is tested using the diff1d-halfsine problem.
+
 from math import pi, sin, cos
 
 if __name__ == '__main__':
@@ -183,23 +214,23 @@ if __name__ == '__main__':
                  [[0.970806, 0], [None, None]]]
     del2bc_ref = [[[0, 0], [0, 0]],
                   [[-9.38655, 0], [None, None]]]
-    A_ref = 0.475228
-    delA_ref = [0.485403, -0.951057]
-    del2A_ref = [-4.69328, 0]
-    P_ref = 0.12
-    delP_ref = [0.1, 0.24]
-    del2P_ref = [-1, 0]
-    Yt_ref = 0.547528
-    delYt_ref = [0.629403, -0.711057]
-    del2Yt_ref = [-5.14008, 0.4104]
+    A_ref = 0.561123
+    delA_ref = [0.572775, -0.951057]
+    del2A_ref = [-5.53807, 0]
+    P_ref = 0.0984
+    delP_ref = [0.082, 0.24]
+    del2P_ref = [-0.82, 0]
+    Yt_ref = 0.610323
+    delYt_ref = [0.673799, -0.770049]
+    del2Yt_ref = [-5.77816, 0.368448]
 
     # Test all functions near the center of the domain.
-    xt = [0.4, 0.5]
+    xt_test = [0.4, 0.41]
 
     # Additional test variables.
-    N_ref = 0.6
-    delN_ref = [0.7, 0.8]
-    del2N_ref = [0.11, 0.22]
+    N_test = 0.5
+    delN_test = [0.61, 0.62]
+    del2N_test = [0.71, 0.72]
 
     # Create a new trial function object.
     tf = Diff1DTrialFunction(bcf, delbcf, del2bcf)
@@ -207,76 +238,90 @@ if __name__ == '__main__':
     print("Testing boundary conditions.")
     for i in range(len(tf.bcf)):
         for (j, f) in enumerate(tf.bcf[i]):
-            bc = f(xt)
-            if ((bc_ref[i][j] is not None and not np.isclose(bc, bc_ref[i][j]))
-                or (bc_ref[i][j] is None and bc is not None)):
-                print("ERROR: bc[%d][%d] = %s, vs ref %s" % (i, j, bc, bc_ref[i][j]))
+            bc_test = f(xt_test)
+            if ((bc_ref[i][j] is not None and
+                 not np.isclose(bc_test, bc_ref[i][j]))
+                or (bc_ref[i][j] is None and bc_test is not None)):
+                print("ERROR: bc[%d][%d] = %s, vs ref %s" %
+                      (i, j, bc_test, bc_ref[i][j]))
 
     print("Testing boundary condition gradients.")
     for i in range(len(tf.delbcf)):
         for j in range(len(tf.delbcf[i])):
             for (k, f) in enumerate(tf.delbcf[i][j]):
-                delbc = f(xt)
-                if ((delbc_ref[i][j][k] is not None and not np.isclose(delbc, delbc_ref[i][j][k]))
-                    or (delbc_ref[i][j][k] is None and delbc is not None)):
-                    print("ERROR: delbc[%d][%d][%d] = %s, vs ref %s" % (i, j, k, delbc, delbc_ref[i][j][k]))
+                delbc_test = f(xt_test)
+                if ((delbc_ref[i][j][k] is not None and
+                     not np.isclose(delbc_test, delbc_ref[i][j][k]))
+                    or (delbc_ref[i][j][k] is None and
+                        delbc_test is not None)):
+                    print("ERROR: delbc[%d][%d][%d] = %s, vs ref %s" %
+                          (i, j, k, delbc_test, delbc_ref[i][j][k]))
 
     print("Testing boundary condition Laplacians.")
     for i in range(len(tf.del2bcf)):
         for j in range(len(tf.del2bcf[i])):
             for (k, f) in enumerate(tf.del2bcf[i][j]):
-                del2bc = f(xt)
-                if ((del2bc_ref[i][j][k] is not None and not np.isclose(del2bc, del2bc_ref[i][j][k]))
-                    or (del2bc_ref[i][j][k] is None and del2bc is not None)):
-                    print("ERROR: del2bc[%d][%d][%d] = %s, vs ref %s" % (i, j, k, del2bc, del2bc_ref[i][j][k]))
+                del2bc_test = f(xt_test)
+                if ((del2bc_ref[i][j][k] is not None and
+                     not np.isclose(del2bc_test, del2bc_ref[i][j][k]))
+                    or (del2bc_ref[i][j][k] is None and
+                        del2bc_test is not None)):
+                    print("ERROR: del2bc[%d][%d][%d] = %s, vs ref %s" %
+                          (i, j, k, del2bc_test, del2bc_ref[i][j][k]))
 
     print("Testing boundary condition function.")
-    A = tf.Af(xt)
-    if not np.isclose(A, A_ref):
-        print("ERROR: A = %s, vs ref %s" % (A, A_ref))
+    A_test = tf.Af(xt_test)
+    if not np.isclose(A_test, A_ref):
+        print("ERROR: A = %s, vs ref %s" % (A_test, A_ref))
 
     print("Testing boundary condition function gradient.")
-    delA = tf.delAf(xt)
-    for (i, delAi) in enumerate(delA):
-        if not np.isclose(delAi, delA_ref[i]):
-            print("ERROR: delA[%d] = %s, vs ref %s" % (i, delAi, delA_ref[i]))
+    delA_test = tf.delAf(xt_test)
+    for (i, delA_t) in enumerate(delA_test):
+        if not np.isclose(delA_t, delA_ref[i]):
+            print("ERROR: delA[%d] = %s, vs ref %s" %
+                  (i, delA_t, delA_ref[i]))
 
     print("Testing boundary condition function Laplacian.")
-    del2A = tf.del2Af(xt)
-    for (i, del2Ai) in enumerate(del2A):
-        if not np.isclose(del2Ai, del2A_ref[i]):
-            print("ERROR: del2A[%d] = %s, vs ref %s" % (i, del2Ai, del2A_ref[i]))
+    del2A_test = tf.del2Af(xt_test)
+    for (i, del2A_t) in enumerate(del2A_test):
+        if not np.isclose(del2A_t, del2A_ref[i]):
+            print("ERROR: del2A[%d] = %s, vs ref %s" %
+                  (i, del2A_t, del2A_ref[i]))
 
     print("Testing network coefficient function.")
-    P = tf.Pf(xt)
-    if not np.isclose(P, P_ref):
-        print("ERROR: P = %s, vs ref %s" % (P, P_ref))
+    P_test = tf.Pf(xt_test)
+    if not np.isclose(P_test, P_ref):
+        print("ERROR: P = %s, vs ref %s" % (P_test, P_ref))
 
     print("Testing network coefficient function gradient.")
-    delP = tf.delPf(xt)
-    for (i, delPi) in enumerate(delP):
-        if not np.isclose(delPi, delP_ref[i]):
-            print("ERROR: delP[%d] = %s, vs ref %s" % (i, delPi, delP_ref[i]))
+    delP_test = tf.delPf(xt_test)
+    for (i, delP_t) in enumerate(delP_test):
+        if not np.isclose(delP_t, delP_ref[i]):
+            print("ERROR: delP[%d] = %s, vs ref %s" %
+                  (i, delP_t, delP_ref[i]))
 
     print("Testing network coefficient function Laplacian.")
-    del2P = tf.del2Pf(xt)
-    for (i, del2Pi) in enumerate(del2P):
-        if not np.isclose(del2Pi, del2P_ref[i]):
-            print("ERROR: del2P[%d] = %s, vs ref %s" % (i, del2Pi, del2P_ref[i]))
+    del2P_test = tf.del2Pf(xt_test)
+    for (i, del2P_t) in enumerate(del2P_test):
+        if not np.isclose(del2P_t, del2P_ref[i]):
+            print("ERROR: del2P[%d] = %s, vs ref %s" %
+                  (i, del2P_t, del2P_ref[i]))
 
     print("Testing trial function.")
-    Yt = tf.Ytf(xt, N_ref)
-    if not np.isclose(Yt, Yt_ref):
-        print("ERROR: Yt = %s, vs ref %s" % (Yt, Yt_ref))
+    Yt_test = tf.Ytf(xt_test, N_test)
+    if not np.isclose(Yt_test, Yt_ref):
+        print("ERROR: Yt = %s, vs ref %s" % (Yt_test, Yt_ref))
 
     print("Testing trial function gradient.")
-    delYt = tf.delYtf(xt, N_ref, delN_ref)
-    for (i, delYti) in enumerate(delYt):
-        if not np.isclose(delYti, delYt_ref[i]):
-            print("ERROR: delYt[%d] = %s, vs ref %s" % (i, delYti, delYt_ref[i]))
+    delYt_test = tf.delYtf(xt_test, N_test, delN_test)
+    for (i, delYt_t) in enumerate(delYt_test):
+        if not np.isclose(delYt_t, delYt_ref[i]):
+            print("ERROR: delYt[%d] = %s, vs ref %s" %
+                  (i, delYt_t, delYt_ref[i]))
 
     print("Testing trial function Laplacian.")
-    del2Yt = tf.del2Ytf(xt, N_ref, delN_ref, del2N_ref)
-    for (i, del2Yti) in enumerate(del2Yt):
-        if not np.isclose(del2Yti, del2Yt_ref[i]):
-            print("ERROR: del2Yt[%d] = %s, vs ref %s" % (i, del2Yti, del2Yt_ref[i]))
+    del2Yt_test = tf.del2Ytf(xt_test, N_test, delN_test, del2N_test)
+    for (i, del2Yt_t) in enumerate(del2Yt_test):
+        if not np.isclose(del2Yt_t, del2Yt_ref[i]):
+            print("ERROR: del2Yt[%d] = %s, vs ref %s" %
+                  (i, del2Yt_t, del2Yt_ref[i]))
