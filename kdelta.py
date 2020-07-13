00f899b57048cb09380ed833555ee80c6829c9e7
Eric Winter <ewinter@gojira.local.stsci.edu>
2020-07-13 12:34:26 -0400

    Added more kdelta tests.

--- kdelta.py
+++ kdelta.py
@@ -1,19 +1,15 @@
 """
 kdelta - Python module to implement the Kronecker delta function
 
-This module provides the Kronecker delta function.
+This module provides the Kronecker delta function:
+
+kdelta(i, j) = 1 if i == j, else 0
 
 Example:
     Calculate the Kronecker delta for 2 integers.
         kd = kdelta(i, j)
 
 Attributes:
-    None
-
-Methods:
-    None
-
-Todo:
     None
 """
 
@@ -24,10 +20,10 @@ def kdelta(i, j):
 
 
 if __name__ == '__main__':
-    for i in range(3):
-        for j in range(3):
+    for i in range(-2, 3):
+        for j in range(-2, 3):
+            print("Testing kdelta[%d,%d]." % (i, j))
             kd = kdelta(i, j)
-            print(i, j, kd)
             if i == j:
                 assert kd == 1
             else:
