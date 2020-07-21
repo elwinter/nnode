f9a3f6f3a4603943e9227b6798e070456a3f611d
Eric Winter <ewinter@gojira.local.stsci.edu>
2020-07-20 21:23:03 -0400

    Moved lagaris_01.py back to eq/ subdirectory.

--- eq/lagaris_01.py
+++ eq/lagaris_01.py
@@ -1,9 +1,10 @@
 """
-Problem 1 in Lagaris et al, 1998 (1st order ODE IVP)
+Problem 1 in Lagaris et al (1998) (1st order ODE IVP)
 
-Note that an upper-case 'Y' is used to represent the Greek psi from the original equation.
+Note that an upper-case 'Y' is used to represent the Greek psi from
+the original equation.
 
-The equation is defined on the domain [0,1]:
+The equation is defined on the domain [0,1].
 
 The analytical form of the equation is:
     G(x, Y, dY/dx) = dY/dx + (x + (1 + 3*x**2)/(1 + x + x**3))*Y
@@ -32,32 +33,32 @@ import numpy as np
 ic = 1
 
 
-def Gf(x, Y, dY_dx):
+def G(x, Y, dY_dx):
     """Code for differential equation"""
     return (
         dY_dx + (x + (1 + 3*x**2)/(1 + x + x**3))*Y
         - x**3 - 2*x - x**2*(1 + 3*x**2)/(1 + x + x**3)
     )
 
-def dG_dYf(x, Y, dY_dx):
+def dG_dY(x, Y, dY_dx):
     """Derivative of G wrt Y"""
     return x + (1 + 3*x**2)/(1 + x + x**3)
 
-def dG_ddYdxf(x, y, dY_dx):
+def dG_ddYdx(x, y, dY_dx):
     """Derivative of G wrt dy/dx"""
     return 1
 
-def Yaf(x):
+def Ya(x):
     """Analytical solution"""
     return exp(-x**2/2)/(1 + x + x**3) + x**2
 
-def dYa_dxf(x):
+def dYa_dx(x):
     """Derivative of analytical solution"""
     return 2*x - exp(-x**2/2)*(1 + x + 4*x**2 + x**4)/(1 + x + x**3)**2
 
 if __name__ == '__main__':
-    assert Gf(0, 0, 0) == 0
-    assert dG_dYf(0, 0, 0) == 1
-    assert dG_ddYdxf(0, 0, 0) == 1
-    assert np.isclose(Yaf(0), ic)
-    assert np.isclose(dYa_dxf(0), -1)
+    assert G(0, 0, 0) == 0
+    assert dG_dY(0, 0, 0) == 1
+    assert dG_ddYdx(0, 0, 0) == 1
+    assert np.isclose(Ya(0), ic)
+    assert np.isclose(dYa_dx(0), -1)
