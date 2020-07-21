1b9864bcf4b8bd6af854422b2ff67d84f6a03a9f
Eric Winter <ewinter@gojira.local.stsci.edu>
2020-07-20 21:15:45 -0400

    Began conversion to use naming conventions and generalized code.

--- pde2bvp.py
+++ pde2bvp.py
@@ -1,20 +1,66 @@
-# N.B. FOR 2-D PDEs ONLY!
+"""
+PDE2BVP - Base class for 2nd-order partial differential equation boundary-
+value problems
+
+This module provides the base functionality for all 2nd-order partial
+differential equation boundary value problem objects used in the nnode software.
+
+Notes:
+
+Y(xv) is the solution to the differential equation (Y is used in place
+of the Greek letter psi).
+
+xv is a vector of independent variables,
+
+m is the number of independent variables.
+
+Example:
+    Create an empty PDE2BVP object.
+        pde2bvp = PDE2BVP()
+    Create an PDE2BVP object from a Python module.
+        pde2bvp = PDEBIVP(modname)
+
+Attributes:
+    name - String containing name of equation definition module
+    Gf - Function for equation
+    dG_dYf - Function for derivative of Gf wrt Y
+    dG_ddelYf - mx1 array of functions for derivatives of Gf wrt dY/dx[j]
+    dG_ddeldelYf - mxm array of functions for derivatives of Gf wrt d2Y/dx[jj]dx[j]
+    bcf - mx2 rray of functions for boundary conditions on Y(xv)
+    delbcf - mx2xm array of functions for boundary condition gradients on Y(xv)
+    deldelbcf - mx2xmxm array of functions for boundary condition
+    gradient gradients on Y(xv)
+    Yaf - (Optional) function for analytical solution Ya(x)
+    delYaf - (Optional) mx1 rray of functions for analytical gradients of Ya[xv]
+    deldelYaf - (Optional) mxm array of functions for analytical
+    gradient gradients of Ya[xv]
+
+Methods:
+
+Todo:
+"""
 
 from importlib import import_module
 
 from pde2 import PDE2
 
 class PDE2BVP(PDE2):
+    """Base class for all 2nd-order partial differential equation boundary-
+    value problem objects"""
 
-    def __init__(self, diffeqmod = None):
-        """Create a PDE2BVP object from a Python module."""
+    def __init__(self, diffeqmod=None):
+        """
+        Constructor
+        Parameters:
+        diffeqmod - The name of the Python module containing the problem definition.
+        """
         self.Gf = None
         self.dG_dYf = None
         self.dG_ddelYf = None
         self.dG_ddeldelYf = None
         self.bcf = None
-        self.bcdf = None
-        self.bcd2f = None
+        self.delbcf = None
+        self.deldelbcf = None
         self.Yaf = None
         self.delYaf = None
         self.deldelYaf = None
@@ -25,28 +71,22 @@ class PDE2BVP(PDE2):
             assert pdemod.dG_ddelYf   # dG/dgrad(Y)
             assert pdemod.dG_ddeldelYf  # dG/d(grad(Y)**2)
             assert pdemod.bcf         # Array of initial condition functions
-            assert pdemod.bcdf        # Array of initial condition derivatives
-            assert pdemod.bcd2f      # Array of initial condition 2nd derivatives
-            # assert pdemod.Yaf         # Analytical solution
-            # assert pdemod.delYaf      # Analytical gradient
-            # assert pdemod.deldelYaf      # Analytical Hessian
-            assert len(pdemod.dG_ddelYf) == 2  # HACK
-            assert len(pdemod.dG_ddeldelYf) == 2  # HACK
-            assert len(pdemod.bcf) == 2        # HACK
-            assert len(pdemod.bcdf) == 2       # HACK
-            assert len(pdemod.bcd2f) == 2       # HACK
-            # assert len(pdemod.delYaf) == 2     # HACK
+            assert pdemod.delbcf        # Array of initial condition derivatives
+            assert pdemod.deldelbcf      # Array of initial condition 2nd derivatives
             self.Gf = pdemod.Gf
             self.dG_dYf = pdemod.dG_dYf
             self.dG_ddelYf = pdemod.dG_ddelYf
             self.dG_ddeldelYf = pdemod.dG_ddeldelYf
             self.bcf = pdemod.bcf
-            self.bcdf = pdemod.bcdf
-            self.bcd2f = pdemod.bcd2f
-            self.Yaf = pdemod.Yaf
-            # self.delYaf = pdemod.delYaf
-            # self.deldelYaf = pdemod.deldelYaf
+            self.delbcf = pdemod.delbcf
+            self.deldelbcf = pdemod.deldelbcf
+            if pdemod.Yaf:
+                self.Yaf = pdemod.Yaf
+            if pdemod.delYaf:
+                self.delYaf = pdemod.delYaf
+            if pdemod.deldelYaf:
+                self.deldelYaf = pdemod.deldelYaf
 
 if __name__ == '__main__':
-    pde2bvp = PDE2BVP('pde02bvp')
+    pde2bvp = PDE2BVP('eq.lagaris_05')
     print(pde2bvp)
